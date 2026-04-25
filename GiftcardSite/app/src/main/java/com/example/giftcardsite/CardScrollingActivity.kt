02b94d184ab717c9fb979be4f743473c63cc61b7
lg3878 <lg3878@nyu.edu>
2026-04-25 12:07:35 -0400

    Comment out Location Manager import in CardScrollingActivity

--- GiftcardSite/app/src/main/java/com/example/giftcardsite/CardScrollingActivity.kt
+++ GiftcardSite/app/src/main/java/com/example/giftcardsite/CardScrollingActivity.kt
@@ -10,7 +10,7 @@ import android.hardware.SensorEvent
 import android.hardware.SensorManager
 import android.location.Location
 import android.location.LocationListener
-import android.location.LocationManager
+//import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
